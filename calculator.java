e90a578c4bfcbb9312b568f5cc4350365973580a
Mpho Dave <[mothibimphodavid@gmail.com]>
2022-01-13 12:54:30 +0200

    Wisdom

--- calculator.java
+++ calculator.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
 
     char operator;
-    int number3, number2, result;
+    double number3, number2, result;
 
     // create an object of Scanner class
     Scanner input = new Scanner(System.in);
